ed3f4754e90af9c476e6999f72a2972031e185a1
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-08-27 23:31:38 -0300

    Update Exercício 04.lua

--- Relatórios/Relatório 2/Exercício 04.lua
+++ Relatórios/Relatório 2/Exercício 04.lua
@@ -1,1 +1,37 @@
+function calcularMedia(a, b)
+    return (a + b) / 2
+end
 
+function encontrarMaior(a, b)
+    if a > b then
+        return a
+    else
+        return b
+    end
+end
+
+function calcularDiferencaAbsoluta(a, b)
+    return math.abs(a - b)
+end
+
+function analisarNumeros(n1, n2, operacao)
+    if operacao == "media" then
+        return calcularMedia(n1, n2)
+    elseif operacao == "maior" then
+        return encontrarMaior(n1, n2)
+    elseif operacao == "diferenca" then
+        return calcularDiferencaAbsoluta(n1, n2)
+    else
+        return "Operação inválida!"
+    end
+end
+
+print("Digite o primeiro número")
+local numero1 = tonumber(io.read())
+print("Digite o segundo número")
+local numero2 = tonumber(io.read())
+print("Digite a operação (\"media\", \"maior\" ou \"diferenca\"):")
+local op = io.read()
+
+local resultado = analisarNumeros(numero1, numero2, op)
+print("Resultado " .. resultado)
