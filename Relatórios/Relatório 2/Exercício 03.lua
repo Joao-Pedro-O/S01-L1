5b6657d20a01106c8a3935dff052127cdaf5016c
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-08-27 22:57:50 -0300

    Update Exercício 03.lua

--- Relatórios/Relatório 2/Exercício 03.lua
+++ Relatórios/Relatório 2/Exercício 03.lua
@@ -1,1 +1,28 @@
+print("Digite a quantidade de elementos")
+local N = tonumber(io.read())
+local tabela = {}
 
+for i = 1, N do
+    print("Digite o elemento " .. i)
+    tabela[i] = tonumber(io.read())
+end
+
+print("Digite o valor limite")
+local K = tonumber(io.read())
+
+function filtrarMaiores(tabela, limite)
+    local tabela2 = {}
+    for i = 1, #tabela do
+        if tabela[i] > limite then
+            table.insert(tabela2, tabela[i])
+        end
+    end
+    return tabela2
+end
+
+local maiores = filtrarMaiores(tabela, K)
+
+print("Elementos maiores que " .. K)
+for i = 1, #maiores do
+    print(maiores[i])
+end
