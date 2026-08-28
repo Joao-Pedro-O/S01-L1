6602777d5215148cc9dad9861e74b14d1f9b3e3d
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-08-27 22:13:25 -0300

    Update Exercício 01.lua

--- Relatórios/Relatório 2/Exercício 01.lua
+++ Relatórios/Relatório 2/Exercício 01.lua
@@ -1,1 +1,14 @@
+print("Digite o expoente inicial")
+local M = tonumber(io.read())
+print("Digite o expoente final")
+local N = tonumber(io.read())
+print("Digite a base")
+local BASE = tonumber(io.read())
 
+function gerarTabelaPotencias(inicio, fim, base)
+    for i = inicio, fim do
+        print(base .. " ^ " .. i .. " = " .. (base^i))
+    end
+end
+
+gerarTabelaPotencias(M, N, BASE)
