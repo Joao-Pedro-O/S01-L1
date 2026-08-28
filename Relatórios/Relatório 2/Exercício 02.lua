480add726d26b37e0c75c9af9458d5d619df4c8f
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-08-27 22:37:31 -0300

    Update Exercício 02.lua

--- Relatórios/Relatório 2/Exercício 02.lua
+++ Relatórios/Relatório 2/Exercício 02.lua
@@ -1,1 +1,23 @@
+print("Digite a quantidade de elementos")
+local N = tonumber(io.read())
+local tabela = {}
+local j = 0
 
+for i = 1, N do
+    print("Digite o elemento " .. i)
+    tabela[i] = tonumber(io.read())
+end
+
+print("Digite o número X a ser buscado")
+local X = tonumber(io.read())
+
+function contarOcorrencias(tabela, alvo)
+    for i = 1, #tabela do
+        if tabela[i] == alvo then
+            j = j + 1
+        end
+    end
+    print("O número " .. X .. " aparece " .. j .. " vez(es) na tabela. ")
+end
+
+contarOcorrencias(tabela, X)
