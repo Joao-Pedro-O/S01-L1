505b04e3ddd33b3f8e7af283cc8e4fb7a5ba6160
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-09-10 20:48:41 -0300

    Update Exercício 02.go

--- Relatórios/Relatório 4/Exercício 02.go
+++ Relatórios/Relatório 4/Exercício 02.go
@@ -1,1 +1,37 @@
+package main
+import "fmt"
 
+func main() {
+	var valor1, valor2, valor3 int
+
+	fmt.Print("Digite as vendas do 1º trimestre: ")
+	fmt.Scanln(&valor1)
+	fmt.Print(valor1,"\n")
+
+	fmt.Print("Digite as vendas do 2º trimestre: ")
+	fmt.Scanln(&valor2)
+	fmt.Print(valor2,"\n")
+
+	fmt.Print("Digite as vendas do 3º trimestre: ")
+	fmt.Scanln(&valor3)
+	fmt.Print(valor3,"\n")
+
+	total := valor1 + valor2 + valor3
+
+	fmt.Printf("Total de vendas: %d unidades\n", total)
+
+	if total < 100 {
+		fmt.Println("Meta mínima anual não atingida!")
+	} else {
+		switch {
+		case total >= 250:
+			fmt.Println("Classificação: Categoria Top Seller")
+
+		case total >= 180:
+			fmt.Println("Classificação: Categoria Sênior")
+
+		case total >= 100:
+			fmt.Println("Classificação: Categoria Pleno")
+		}
+	}
+}
