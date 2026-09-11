86918fc8534c5216b4d4c208f2d7395ce41102cc
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-09-10 23:05:26 -0300

    Update Exercício 04.go

--- Relatórios/Relatório 4/Exercício 04.go
+++ Relatórios/Relatório 4/Exercício 04.go
@@ -1,1 +1,33 @@
+package main
+import "fmt"
 
+func validarIngresso(setor string, codigo int) bool {
+	if setor == "VIP" && codigo == 2026 {
+		return true
+	}
+	return false
+}
+
+func main() {
+	var setor string
+	var codigo int
+
+	for {
+		fmt.Print("Digite o setor do ingresso: ")
+		fmt.Scan(&setor)
+		fmt.Print(setor,"\n")
+
+		fmt.Print("Digite o código do ingresso: ")
+		fmt.Scan(&codigo)
+		fmt.Print(codigo,"\n")
+
+		valido := validarIngresso(setor, codigo)
+
+		if valido {
+			fmt.Println("Acesso liberado à área VIP!")
+			break
+		} else {
+			fmt.Println("Ingresso ou setor inválido. Tente novamente.")
+		}
+	}
+}
