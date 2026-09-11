e92018cdc1115507d6211b42c0596a51276644bd
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-09-10 22:20:29 -0300

    Update Exercício 03.go

--- Relatórios/Relatório 4/Exercício 03.go
+++ Relatórios/Relatório 4/Exercício 03.go
@@ -1,1 +1,21 @@
+package main
+import "fmt"
 
+func gerarEscalaPlantao(n int) {
+	fmt.Println("--- Escala de Plantão Técnico ---")
+
+	dia := 1
+	for i := 1; i <= n; i++ {
+		fmt.Printf("Plantão %d: Dia %d do mês\n", i, dia)
+		dia = dia + 4
+	}
+}
+
+func main() {
+	var n int
+	fmt.Print("Digite a quantidade de plantões necessários: ")
+	fmt.Scan(&n)
+	fmt.Print(n,"\n")
+
+	gerarEscalaPlantao(n)
+}
