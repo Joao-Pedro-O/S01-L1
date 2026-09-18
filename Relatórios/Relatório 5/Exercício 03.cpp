2408434f61c0aea1be0ee43e3c8de2bddfc0a153
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-09-18 07:13:10 -0300

    Update Exercício 03.cpp

--- Relatórios/Relatório 5/Exercício 03.cpp
+++ Relatórios/Relatório 5/Exercício 03.cpp
@@ -2,7 +2,8 @@
 #include <iomanip>
 using namespace std;
 
-int main() {
+int main() 
+{
     float capacidadeMaxima;
     cout << "Informe a capacidade maxima de carga do drone (kg): ";
     cin >> capacidadeMaxima;
@@ -10,7 +11,8 @@ int main() {
     float cargaAtual = 0.0;
     int opcao;
 
-    do {
+    do 
+    {
         cout << "=== SISTEMA DE CARGA DO DRONE ===" << endl;
         cout << "1. Verificar Carga" << endl;
         cout << "2. Carregar Pacote" << endl;
@@ -19,40 +21,51 @@ int main() {
         cout << "Escolha uma opcao: ";
         cin >> opcao;
 
-        if (opcao == 1) {
+        if (opcao == 1) 
+        {
             float espacoDisponivel = capacidadeMaxima - cargaAtual;
             cout << fixed << setprecision(2);
             cout << "Carga Atual: " << cargaAtual << " kg / " << capacidadeMaxima << " kg" << endl;
             cout << "Espaco Disponivel: " << espacoDisponivel << " kg" << endl;
         }
-        else if (opcao == 2) {
+        else if (opcao == 2) 
+        {
             float pesoPacote;
             cout << "Digite o peso do pacote a ser carregado (kg): ";
             cin >> pesoPacote;
 
-            if (cargaAtual + pesoPacote > capacidadeMaxima) {
+            if (cargaAtual + pesoPacote > capacidadeMaxima) 
+            {
                 cout << "Alerta: Peso maximo de decolagem excedido! Operacao cancelada." << endl;
-            } else {
+            } 
+            else 
+            {
                 cargaAtual = cargaAtual + pesoPacote;
                 cout << "Pacote adicionado com sucesso!" << endl;
             }
         }
-        else if (opcao == 3) {
+        else if (opcao == 3) 
+        {
             float pesoRemover;
             cout << "Digite o peso a ser removido (kg): ";
             cin >> pesoRemover;
 
-            if (pesoRemover > cargaAtual) {
+            if (pesoRemover > cargaAtual) 
+            {
                 cout << "Alerta: Nao e possivel remover mais peso do que o carregado." << endl;
-            } else {
+            } 
+            else 
+            {
                 cargaAtual = cargaAtual - pesoRemover;
                 cout << "Pacote removido com sucesso!" << endl;
             }
         }
-        else if (opcao == 4) {
+        else if (opcao == 4) 
+        {
             cout << "Encerrando sistema de telemetria..." << endl;
         }
-        else {
+        else 
+        {
             cout << "Opcao invalida. Tente novamente." << endl;
         }
 
