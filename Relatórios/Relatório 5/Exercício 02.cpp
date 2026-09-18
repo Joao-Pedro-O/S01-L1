b798e9f910cbb95743409760c92f600d41a067c5
Joao-Pedro-O <150342408+Joao-Pedro-O@users.noreply.github.com>
2026-09-18 07:10:57 -0300

    Update Exercício 02.cpp

--- Relatórios/Relatório 5/Exercício 02.cpp
+++ Relatórios/Relatório 5/Exercício 02.cpp
@@ -1,22 +1,26 @@
 #include <iostream>
 using namespace std;
 
-float calcular_confiabilidade_sistema(float probabilidades[], int tamanho) {
+float calcular_confiabilidade_sistema(float probabilidades[], int tamanho) 
+{
     float confiabilidade = 1.0;
-    for (int i = 0; i < tamanho; i++) {
+    for (int i = 0; i < tamanho; i++) 
+    {
         confiabilidade = confiabilidade * probabilidades[i];
     }
     return confiabilidade;
 }
 
-int main() {
+int main() 
+{
     int n;
     cout << "Digite a quantidade de componentes do sistema: ";
     cin >> n;
 
     float* probabilidades = new float[n];
 
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) 
+    {
         cout << "Digite a probabilidade do componente " << (i + 1) << " (ex: 0.95): ";
         cin >> probabilidades[i];
     }
